ac578f75722e7a83ed2ddd77ded59086313ad79f
xiongneng <xiongneng@HDFAX.com>
2018-05-10 14:46:43 +0800

    update

--- picenter/piweb/src/main/java/org/xione/piweb/controller/HelloController.java
+++ picenter/piweb/src/main/java/org/xione/piweb/controller/HelloController.java
@@ -1,14 +1,19 @@
 package org.xione.piweb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
 	@RequestMapping("/hello")
 	public String sayHi() {
-		return "Hello mv";
+		LOG.info("helloCOntroller_sayHi");
+		return "Hello mv" + System.currentTimeMillis();
 	}
 
 }
