9f2289bf8955cb0aa497823953487381fceaa230
xiongneng <xiongneng@HDFAX.com>
2018-05-15 12:06:55 +0800

    update

---
+++ basicenter/xiserver/src/test/java/org/xione/xiserver/XiserverApplicationTests.java
@@ -0,0 +1,16 @@
+package org.xione.xiserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XiserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
